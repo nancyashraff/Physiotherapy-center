6c16f8335d75c4a1661e3a9d2019895f7196e72a
maryam-refaat <mariammazhar15@gmail.com>
2025-04-13 05:22:28 +0200

    Update EarlyPList.h

--- EarlyPList.h
+++ EarlyPList.h
@@ -2,20 +2,27 @@
 
 #include "PriQueue.h"
 #include "Patient.h"
-
+class Patient;
 class EarlyPList : public PriQueue<Patient*> 
 {
     int Presc; // Rescheduling probability (0-100)
+    int EarlyNumber;
+    int ResceduleNumber;
 public:
-<<<<<<< HEAD
-    EarlyPList() : Presc(0) {}
-    EarlyPList(int rescheduleProb) : Presc(rescheduleProb) {}
-=======
     EarlyPList(int rescheduleProb=0) : Presc(rescheduleProb),EarlyNumber(0),ResceduleNumber(0) {}
->>>>>>> 5c7257d3bd145d8168ced912365aad9ad01cd9c5
 
     Patient* reschedule();
+
+    void enqueue(Patient* &data, int priority);
+    
+    float getReschedulePercent(int &LateNumber);
+    float getEarlyPercent(int &LateNumber);
+
+    int GetEarlyNumber();
 };
+
+
+
 
 
 
