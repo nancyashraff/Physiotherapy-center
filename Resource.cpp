98376b36d936a67df24fe6b0449cc8ba0005d463
nancyashraff <nancy.sabry05@eng-st.cu.edu.eg>
2025-04-15 22:52:21 +0200

    Delete Resource.cpp

--- Resource.cpp
+++
@@ -1,9 +0,0 @@
-#include "Resource.h"
-
-Resource::Resource(TreatmentType t,int c) :  type(t), id(next_id++), capacity(c)
-{
-}
-int Resource::get_id(){
-    return id;
-}
-int Resource::getCapacity() const { return capacity; }
